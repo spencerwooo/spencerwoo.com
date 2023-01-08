ac7316f23df21e9289b35b6f1aa246ad3a8113bf
spencerwooo <spencer.woo@outlook.com>
2023-01-08 14:07:59 +0800

    remove newsletter

--- pages/index.tsx
+++ pages/index.tsx
@@ -94,7 +94,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter.provider && (
+      {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
